0da8287b718c8df4518501629e51dbceb4d7b9a2
MarcoBagnasco <bagna.hbk@gmail.com>
2021-05-18 14:14:32 +0200

    add snack 1

--- index.php
+++ index.php
@@ -8,5 +8,50 @@
 </head>
 <body>
     <h1>PHP Snacks</h1>
+
+    <!-- SNACK 1 -->
+    <h2>Snack 1</h2>
+
+    <?php 
+    $matches = [
+        [
+            'home_team' => 'Nets',
+            'away_team' => 'Cavaliers',
+            'home_point' => 123,
+            'away_point' => 109,
+        ],
+        [
+            'home_team' => 'Hawks',
+            'away_team' => 'Rockets',
+            'home_point' => 124,
+            'away_point' => 95,
+        ],
+        [
+            'home_team' => 'Thunder',
+            'away_team' => 'Clippers',
+            'home_point' => 117,
+            'away_point' => 112,
+        ],
+        [
+            'home_team' => 'Pelicans',
+            'away_team' => 'Lakers',
+            'home_point' => 98,
+            'away_point' => 110,
+        ],
+        [
+            'home_team' => 'Kings',
+            'away_team' => 'Jazz',
+            'home_point' => 99,
+            'away_point' => 121,
+        ],
+    ];
+
+    for($i = 0; $i < count($matches); $i++) { ?>
+    <ul>
+        <li><?php echo $matches[$i]['home_team'] . ' - '
+        . $matches[$i]['away_team'] . ' | ' . 
+        $matches[$i]['home_point'] . '-' . $matches[$i]['away_point']?></li>
+    </ul>
+    <?php } ?>
 </body>
 </html>
